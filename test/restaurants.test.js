35f60b95d52031bff7c0f2c8baa9939a8997811d
ckrountree <jmail0207@gmail.com>
2017-12-03 15:44:33 -0800

    pass getAll restaurants test

--- test/restaurants.test.js
+++ test/restaurants.test.js
@@ -16,6 +16,15 @@ describe('Restaurants API', () => {
         }]
     };
 
+    const kensArtisanPizza = {
+        name: 'kens',
+        cuisine: 'northwest',
+        reviews: [{
+            rating: 4,
+            comments: 'just like I had in Napoli',
+            email: 'tav@me.com'
+        }]
+    };
 
     it('saves a restaurant', () => {
         return request.post('/api/restaurants')
@@ -24,5 +33,21 @@ describe('Restaurants API', () => {
             .then(Restaurant =>  {
                 assert.deepEqual(Restaurant.name, bambooSushi.name);
             });
+    });
+    
+    it('gets all restaurants', () => {
+        const allRestaurants = [bambooSushi, kensArtisanPizza];
+        return Promise.all(allRestaurants.map(restaurant => {
+            return request.post('/api/restaurants')
+                .send(restaurant)
+                .then(res => res.body);
+        }))
+            .then(() => {
+                return request.get('/api/restaurants')
+                    .then(restaurants => {
+                        assert.deepEqual(restaurants.body.length, 2);
+                    });
+            });
+        
     });
 });
